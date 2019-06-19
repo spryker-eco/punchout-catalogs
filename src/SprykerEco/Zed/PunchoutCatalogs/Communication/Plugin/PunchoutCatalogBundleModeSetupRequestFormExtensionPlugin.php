1fbfbf1645c05b99efbdb4475255ab333c36a067
Anton Smarovydlo <anton.smarovydlo@spryker.com>
2019-06-19 12:10:23 +0300

    PS-9536: amendments after review

--- src/SprykerEco/Zed/PunchoutCatalogs/Communication/Plugin/PunchoutCatalogBundleModeSetupRequestFormExtensionPlugin.php
+++ src/SprykerEco/Zed/PunchoutCatalogs/Communication/Plugin/PunchoutCatalogBundleModeSetupRequestFormExtensionPlugin.php
@@ -9,7 +9,7 @@ namespace SprykerEco\Zed\PunchoutCatalogs\Communication\Plugin;
 
 use SprykerEco\Zed\PunchoutCatalogs\Communication\Form\PunchoutCatalogSetupRequestBundleModeForm;
 
-class PunchoutCatalogSetupRequestBundleModeFormFieldPlugin implements PunchoutCatalogSetupRequestFormExtensionPluginInterface
+class PunchoutCatalogBundleModeSetupRequestFormExtensionPlugin implements PunchoutCatalogSetupRequestFormExtensionPluginInterface
 {
     /**
      * {@inheritdoc}
